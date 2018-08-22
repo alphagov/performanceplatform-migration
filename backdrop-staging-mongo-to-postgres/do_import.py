9a606ebf39c0be2368a3b50a48316390ef1e0731
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-22 20:53:50 +0100

    change schema, add indices, make better
    - schema should have updated_at and timestamp as columns
    - collection, updated_at, timestamp should be fast to access
    - import workers should shuffle their collections so that if they crash
    they get through _more_ collections

--- backdrop-staging-mongo-to-postgres/do_import.py
+++ backdrop-staging-mongo-to-postgres/do_import.py
@@ -26,10 +26,27 @@ PSQL_CONN = psycopg2.connect(PSQL_URI)
 with PSQL_CONN.cursor() as psql_cursor:
     psql_cursor.execute("""
     CREATE TABLE IF NOT EXISTS mongo (
-      id         VARCHAR PRIMARY KEY,
-      collection VARCHAR NOT NULL,
-      record     JSON NOT NULL
+      id         VARCHAR   PRIMARY KEY,
+      collection VARCHAR   NOT NULL,
+      timestamp  TIMESTAMP NOT NULL,
+      updated_at TIMESTAMP NOT NULL,
+      record     JSON      NOT NULL
     )
+  """)
+    PSQL_CONN.commit()
+
+    psql_cursor.execute("""
+    CREATE INDEX IF NOT EXISTS mongo_collection ON mongo (collection)
+  """)
+    PSQL_CONN.commit()
+
+    psql_cursor.execute("""
+    CREATE INDEX IF NOT EXISTS mongo_timestamp ON mongo (timestamp)
+  """)
+    PSQL_CONN.commit()
+
+    psql_cursor.execute("""
+    CREATE INDEX IF NOT EXISTS mongo_updated_at ON mongo (updated_at)
   """)
     PSQL_CONN.commit()
 PSQL_CONN.close()
@@ -48,7 +65,8 @@ COLLECTIONS_PER_WORKER = len(COLLECTIONS) // WORKER_COUNT
 WORKER_COLL_START = COLLECTIONS_PER_WORKER * WORKER_INDEX
 WORKER_COLL_END = COLLECTIONS_PER_WORKER * (WORKER_INDEX + 1)
 
-WORKER_COLLECTIONS = shuffle(COLLECTIONS[WORKER_COLL_START:WORKER_COLL_END])
+WORKER_COLLECTIONS = COLLECTIONS[WORKER_COLL_START:WORKER_COLL_END]
+shuffle(WORKER_COLLECTIONS)
 
 for index, cname in enumerate(WORKER_COLLECTIONS):
     num_items_in_collection = DB[cname].find().count()
@@ -60,19 +78,23 @@ for index, cname in enumerate(WORKER_COLLECTIONS):
             records = DB[cname].find().limit(CHUNK_SIZE).skip(chunks[ci])
             records_str = [
                 psql_cursor.mogrify(
-                    "(%s, %s, %s)",
+                    "(%s, %s, %s, %s, %s)",
                     (
                         '{}:{}'.format(cname, r['_id']),
                         cname,
+                        r['_timestamp'],
+                        r['_updated_at'],
                         json.dumps(r, default=to_json)
                     )
                 )
                 for r in records
                 if '_id' in r
+                if '_updated_at' in r
+                if '_timestamp' in r
             ]
 
             query = """INSERT INTO mongo
-                       (id, collection, record)
+                       (id, collection, timestamp, updated_at, record)
                        VALUES """ + ",".join(records_str) + " ON CONFLICT DO NOTHING"
             psql_cursor.execute(query)
             psql_conn.commit()
